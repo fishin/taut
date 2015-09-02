7e1b9481cb8612319ef3cf327a230e188eabfba5
lloydbenson <lloyd.benson@gmail.com>
2015-09-01 19:38:28 -0500

    fix top same job block

--- test/pr.js
+++ test/pr.js
@@ -97,6 +97,46 @@ describe('pr', function () {
         });
     });
 
+    it('startPR 1:2 with running pr 1:1 and pr 1:1 at top', function (done) {
+
+        var taut = new Taut(internals.defaults);
+        taut.settings.size = 2;
+        taut.settings.getActivePullRequests = function () {
+
+            return {
+                '1': {
+                    prs: {
+                        '1': {}
+                    }
+                }
+            };
+        };
+        var queueObj = taut.startQueue();
+        var pr = {
+            number: 1
+        };
+        taut.settings.startJob('1', pr, function () {
+
+            taut.addJob('1', pr);
+            var queue = taut.getQueue();
+            expect(queue.length).to.equal(1);
+            taut.addJob('1', { number: 2 });
+            queue = taut.getQueue();
+            expect(queue.length).to.equal(2);
+            var intervalObj = setInterval(function () {
+
+                queue = taut.getQueue();
+                if (queue.length === 1) {
+                    clearInterval(intervalObj);
+                    taut.removeJob('1', 2);
+                    taut.removeJob('1', 1);
+                    taut.stopQueue(queueObj);
+                    done();
+                }
+            }, 1000);
+        });
+    });
+
     it('addPR 1:1 with existing 1:1 queue', function (done) {
 
         var taut = new Taut(internals.defaults);
